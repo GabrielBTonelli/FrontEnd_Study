f935f5ac4c349fdbc65b2b4588c5de29c74741dc
GabrielBTonelli <tonelli.gabriel@gmail.com>
2023-04-10 11:13:51 +0200

    EndOfCourse

---
+++ Errors and Bugs.js
@@ -0,0 +1,10 @@
+// console.log(a + b);
+// console.log("This line is never reached")
+
+// throw new ReferenceError();
+
+try {
+    Number(5).toPrecision(300)
+    } catch(e) {
+    console.log("There was an error")
+    }
